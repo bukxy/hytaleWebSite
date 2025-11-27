6429f992e607171f0828ea95b620e2b5a94b1476
bukxy <peron.nicolas2@gmail.com>
2025-11-27 21:41:24 +0100

    feat: Add VoteWebsites mfsc

---
+++ app/Http/Controllers/VotesWebsitesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class VotesWebsitesController extends Controller
+{
+
+}
