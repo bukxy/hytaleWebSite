7a30739d4664ed4175f5cb783cbd53e76a5cb16d
bukxy <peron.nicolas2@gmail.com>
2025-11-28 19:22:16 +0100

    refactor: Changed DatabaseSeeder to define in which order the seeders are executed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\VotesWebsites;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,15 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
-        );
+        $this->call([
+            RolesSeeder::class,
+            UserSeeder::class,
+            VotesWebsitesSeeder::class,
+            VoteSeeder::class,
+        ]);
     }
 }
