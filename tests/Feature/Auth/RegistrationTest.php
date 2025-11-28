7d7953c0e31952098d342de055deac8c46015c83
bukxy <peron.nicolas2@gmail.com>
2025-11-28 19:24:52 +0100

    test: Add role_id by default when new user registration

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Roles;
+
 test('registration screen can be rendered', function () {
     $response = $this->get(route('register'));
 
@@ -12,6 +14,7 @@ test('new users can register', function () {
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
+        'role_id' => Roles::factory()->create()->id,
     ]);
 
     $this->assertAuthenticated();
