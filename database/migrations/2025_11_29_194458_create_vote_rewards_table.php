a0296f53bb324da2dfb86d45728c8d7cba1a2be5
bukxy <peron.nicolas2@gmail.com>
2025-11-29 20:53:59 +0100

    feat: Add vote_rewards mfsc

---
+++ database/migrations/2025_11_29_194458_create_vote_rewards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vote_rewards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->decimal('chances');
+            $table->decimal('money')->default(0);
+            $table->text('commands')->nullable();
+            $table->boolean('need_online')->default(false);
+            $table->boolean('is_enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vote_rewards');
+    }
+};
