f6f694e7ce68205952ff05f9cba2e38fc5a1617b
bukxy <peron.nicolas2@gmail.com>
2025-11-29 16:38:32 +0100

    refactor: Back login register button

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -20,53 +20,37 @@ export default function Home({
                     rel="stylesheet"
                 />
             </Head>
-            {/*<div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">*/}
-            {/*    <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">*/}
-            {/*        <nav className="flex items-center justify-end gap-4">*/}
-            {/*            {auth.user ? (*/}
-            {/*                <Link*/}
-            {/*                    href={dashboard()}*/}
-            {/*                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"*/}
-            {/*                >*/}
-            {/*                    Dashboard*/}
-            {/*                </Link>*/}
-            {/*            ) : (*/}
-            {/*                <>*/}
-            {/*                    <Link*/}
-            {/*                        href={login()}*/}
-            {/*                        className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"*/}
-            {/*                    >*/}
-            {/*                        Log in*/}
-            {/*                    </Link>*/}
-            {/*                    {canRegister && (*/}
-            {/*                        <Link*/}
-            {/*                            href={register()}*/}
-            {/*                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"*/}
-            {/*                        >*/}
-            {/*                            Register*/}
-            {/*                        </Link>*/}
-            {/*                    )}*/}
-            {/*                </>*/}
-            {/*            )}*/}
-            {/*        </nav>*/}
-            {/*    </header>*/}
-            {/*</div>*/}
             <AppFrontLayout>
                 <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
-                    </div>
-                    <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
+                    <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
+                        <nav className="flex items-center justify-end gap-4">
+                            {auth.user ? (
+                                <Link
+                                    href={dashboard()}
+                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                >
+                                    Dashboard
+                                </Link>
+                            ) : (
+                                <>
+                                    <Link
+                                        href={login()}
+                                        className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                                    >
+                                        Log in
+                                    </Link>
+                                    {canRegister && (
+                                        <Link
+                                            href={register()}
+                                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                        >
+                                            Register
+                                        </Link>
+                                    )}
+                                </>
+                            )}
+                        </nav>
+                    </header>
                 </div>
             </AppFrontLayout>
         </>
