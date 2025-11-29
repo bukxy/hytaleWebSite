eef40370f4248a18454ea077628872352f78567e
bukxy <peron.nicolas2@gmail.com>
2025-11-29 20:53:07 +0100

    refactor: Move role_id from users to roles

--- database/migrations/2025_11_28_105950_create_roles_table.php
+++ database/migrations/2025_11_28_105950_create_roles_table.php
@@ -20,7 +20,8 @@ return new class extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('roles');
+          $table->unsignedBigInteger('role_id')->default(1);
+          $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
