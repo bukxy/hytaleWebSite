9e778ff9a16efac7a44f0da27e07bd248b586769
bukxy <peron.nicolas2@gmail.com>
2025-11-30 17:13:10 +0100

    feat: Add name/prefix for all route behind the dashboard + Add vote route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VoteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -33,9 +34,19 @@ Route::get('/shop', function () {
  * Dashboard Route
  */
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+
+    Route::name('dashboard_')->group(function () {
+        Route::prefix('dashboard')->group(function () {
+
+            Route::get('/', function () { return Inertia::render('dashboard'); })->name('home');
+
+            Route::prefix('vote', )->group(function () {
+                Route::get('/', [VoteController::class, 'retrieve'])->name('vote');
+            });
+
+        });
+    });
+
 });
 
 require __DIR__.'/settings.php';
