021440e3557e048d6fd3a97245b2b52a03218e27
bukxy <peron.nicolas2@gmail.com>
2025-12-02 15:06:14 +0100

    refactor: Edit table name to plural

--- database/migrations/2025_11_29_194458_create_votes_rewards_table.php
+++ database/migrations/2025_11_29_194458_create_votes_rewards_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vote_rewards', function (Blueprint $table) {
+        Schema::create('votes_rewards', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->decimal('chances');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vote_rewards');
+        Schema::dropIfExists('votes_rewards');
     }
 };
