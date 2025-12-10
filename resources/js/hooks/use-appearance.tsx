564e8ee1c29052c7c93b83ad2a6af50cb05e653d
bukxy <peron.nicolas2@gmail.com>
2025-12-10 17:14:33 +0100

    refactor: back to use localStorage.getItem('appearance')

--- resources/js/hooks/use-appearance.tsx
+++ resources/js/hooks/use-appearance.tsx
@@ -41,8 +41,7 @@ const handleSystemThemeChange = () => {
 };
 
 export function initializeTheme() {
-    const savedAppearance = 'light';
-        // (localStorage.getItem('appearance') as Appearance) || 'system';
+    const savedAppearance = (localStorage.getItem('appearance') as Appearance) || 'system';
 
     applyTheme(savedAppearance);
 
