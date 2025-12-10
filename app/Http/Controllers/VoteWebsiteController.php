fcdd53250460559604e8bd0859da00ee4cab2f82
bukxy <peron.nicolas2@gmail.com>
2025-12-10 17:16:08 +0100

    feat: Add VoteWebsiteController addStore

--- app/Http/Controllers/VoteWebsiteController.php
+++ app/Http/Controllers/VoteWebsiteController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VoteWebsiteCreateRequest;
+use App\Models\VoteWebsite;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -18,20 +21,30 @@ class VoteWebsiteController extends Controller
 
     public function add()
     {
-        return Inertia::render('dashboard/votes_websites/add_edit');
+        return Inertia::render('dashboard/votes_websites/add_edit', [
+            'votes_websites' => VoteWebsite::with(['createdBy', 'updatedBy'])
+                ->get()
+                ->map(function ($vw) {
+                    return [
+                        'id' => $vw->id,
+                        'name' => $vw->name,
+                        'url' => $vw->url,
+                        'created_at' => $vw->created_at,
+                        'created_by' => $vw->createdBy->name,
+                        'updated_at' => $vw->updated_at,
+                        'updated_by' => $vw->updatedBy?->name,
+                        'is_enabled' => $vw->is_enabled,
+                        'has_verification' => $vw->has_verification,
+                    ];
+                }),
+        ]);
     }
 
-    public function addStore(Request $request)
+    public function addStore(VoteWebsiteCreateRequest $request): RedirectResponse
     {
-        // Validate and store the new vote website
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'url' => 'required|url',
-            'description' => 'nullable|string',
-        ]);
 
-        // VoteWebsite::create($validated);
+        VoteWebsite::create($request->validated());
 
-        return redirect()->route('dashboard-vote-website')->with('success', 'Vote website created successfully.');
+        return to_route('dashboard.vote-website.vwAdd')->with('success', 'Vote website created successfully.');
     }
 }
