62a42a05484664c6b83f34247bd91f118eab95e4
bukxy <peron.nicolas2@gmail.com>
2025-12-10 17:12:12 +0100

    feat: Add votes_websites user_id / file_logo_id + add nullable verification_key

--- database/migrations/2025_11_27_174446_create_votes_websites_table.php
+++ database/migrations/2025_11_27_174446_create_votes_websites_table.php
@@ -15,15 +15,21 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('url');
-            $table->string('verification_key');
+            $table->string('verification_key')->nullable();
             $table->boolean('has_verification')->default(true);
             $table->boolean('is_enabled')->default(true);
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
 
             $table->unsignedBigInteger('created_by');
             $table->foreign('created_by')->references('id')->on('users');
 
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->foreign('updated_by')->references('id')->on('users');
+
+            $table->unsignedBigInteger('file_logo_id')->nullable();
+            $table->foreign('file_logo_id')->references('id')->on('files')->cascadeOnDelete();
 
             $table->timestamps();
         });
