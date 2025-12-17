2d8429f2f28d83a7e846881adff361b716ceee15
bukxy <peron.nicolas2@gmail.com>
2025-12-17 14:51:02 +0100

    feat: Complete definition

--- database/factories/VoteWebsiteFactory.php
+++ database/factories/VoteWebsiteFactory.php
@@ -2,12 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\votes_websites>
  */
-class VotesWebsitesFactory extends Factory
+class VoteWebsiteFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +18,13 @@ class VotesWebsitesFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => 'Test Vote Website',
+            'url' => 'https://testvotewebsite.com',
+            'verification_key' => 'testapikey123',
+            'has_verification' => true,
+            'is_enabled' => true,
+            'user_id' => User::factory(),
+            'created_by' => User::factory()
         ];
     }
 }
