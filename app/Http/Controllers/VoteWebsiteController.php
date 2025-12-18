f765699a76af8530236db74fb7a8fbb113c7b6f3
bukxy <peron.nicolas2@gmail.com>
2025-12-18 18:52:16 +0100

    feat: Add edit editStore

--- app/Http/Controllers/VoteWebsiteController.php
+++ app/Http/Controllers/VoteWebsiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\VoteWebsiteCreateRequest;
+use App\Http\Resources\VoteWebsiteResource;
 use App\Models\File;
 use App\Models\VoteWebsite;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -98,6 +99,95 @@ class VoteWebsiteController extends Controller
         }
 
         return to_route('dashboard.vote-website.add')->with('success', 'Vote website created successfully.');
+    }
+
+    public function edit(int $id)
+    {
+        $voteWebsite = VoteWebsite::with(['createdBy', 'updatedBy', 'logo'])->findOrFail($id);
+
+        if (!$voteWebsite)
+            return to_route('dashboard.vote.list')->with('error', 'Vote website does not exist');
+
+        return Inertia::render('dashboard/votes_websites/add_edit', [
+            'data' => (new VoteWebsiteResource($voteWebsite))->resolve(),
+            'votes_websites' => VoteWebsite::with(['createdBy', 'updatedBy'])
+                ->get()
+                ->map(function ($vw) {
+                    return [
+                        'id' => $vw->id,
+                        'name' => $vw->name,
+                        'url' => $vw->url,
+                        'created_at' => $vw->created_at,
+                        'created_by' => $vw->createdBy->name,
+                        'updated_at' => $vw->updated_at,
+                        'updated_by' => $vw->updatedBy?->name,
+                        'is_enabled' => $vw->is_enabled,
+                        'has_verification' => $vw->has_verification,
+                    ];
+                }),
+        ]);
+    }
+
+    public function editStore(VoteWebsiteCreateRequest $request, int $id): RedirectResponse
+    {
+
+        $voteWebsite = VoteWebsite::findOrFail($id);
+
+        $errors = new MessageBag();
+//        dd($request->all());
+        try {
+            DB::transaction(function () use ($voteWebsite, $errors, $request) {
+
+                try {
+                    $voteWebsite->update($request->validated());
+                } catch (\Throwable $e) {
+                    $errors->add('voteWebsite', 'Error update VoteWebsite');
+                    throw $e;
+                }
+
+//                if ($request->file('logo')) {
+//                    try {
+//                        $logoFile = $request->file('logo');
+//                        $filename = Uuid::uuid4()->toString().'.'.$logoFile->extension();
+//
+//                        $path = Storage::disk('public')->putFileAs(
+//                            '',
+//                            $logoFile,
+//                            $filename
+//                        );
+//
+//                        if (! $path) {
+//                            throw new \RuntimeException();
+//                        }
+//
+//                    } catch (\Throwable $e) {
+//                        $errors->add('logo', 'Error upload logo');
+//                        throw $e;
+//                    }
+//
+//                    try {
+//                        $voteWebsite->logo()->create([
+//                            'filename' => $filename,
+//                            'path' => $path,
+//                            'type'     => File::LOGO,
+//                            'user_id'  => auth()->id(),
+//                        ]);
+//                    } catch (\Throwable $e) {
+//                        $errors->add('logo', 'Error create logo');
+//                        throw $e;
+//                    }
+//
+//                }
+            });
+
+        } catch (\Throwable $e) {
+            if (isset($path))
+                Storage::disk('public')->delete($path);
+
+            return back()->withErrors($errors);
+        }
+
+        return to_route('dashboard.vote-website.edit', $voteWebsite->id)->with('success', 'Vote website created successfully.');
     }
 
     public function delete(int $id): RedirectResponse
