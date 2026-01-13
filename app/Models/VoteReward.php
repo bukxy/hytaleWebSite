65024132215cb90188e2155aab5caecf23e05a9c
bukxy <peron.nicolas2@gmail.com>
2026-01-13 15:44:55 +0100

    feat: Add VoteReward model with his fillable and relation

--- app/Models/VoteReward.php
+++ app/Models/VoteReward.php
@@ -11,4 +11,35 @@ class VoteReward extends Model
     use HasFactory;
 
     protected $table = 'votes_rewards';
+
+    protected $fillable = [
+        'name',
+        'chances',
+        'is_enabled',
+        'is_online_required',
+        'money',
+        'commands',
+        'created_by',
+        'updated_by',
+    ];
+
+    protected $casts = [
+        'is_enabled' => 'boolean',
+        'is_online_required' => 'boolean',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function image() {
+        return $this->morphOne(File::class, 'fileable')
+            ->where('type', '=',File::REWARD_IMAGE);
+    }
 }
