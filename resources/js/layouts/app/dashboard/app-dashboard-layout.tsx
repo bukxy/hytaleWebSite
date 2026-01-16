6e967bd11494ec273d55cb4addd059c0883ea521
bukxy <peron.nicolas2@gmail.com>
2026-01-16 14:40:29 +0100

    feat: Add AppDashboardLayout

---
+++ resources/js/layouts/app/dashboard/app-dashboard-layout.tsx
@@ -0,0 +1,17 @@
+import AppDashboardSidebarLayout from '@/layouts/app/dashboard/app-dashboard-sidebar-layout';
+import { type BreadcrumbItem } from '@/types';
+import { type ReactNode } from 'react';
+import {usePage} from "@inertiajs/react";
+
+interface AppLayoutProps {
+    children: ReactNode;
+}
+
+export default function AppDashboardLayout({ children, ...props }: AppLayoutProps) {
+    const { breadcrumbs = [] } = usePage<{ breadcrumbs?: BreadcrumbItem[] }>().props;
+    return (
+        <AppDashboardSidebarLayout breadcrumbs={breadcrumbs} {...props}>
+            {children}sss
+        </AppDashboardSidebarLayout>
+    );
+}
