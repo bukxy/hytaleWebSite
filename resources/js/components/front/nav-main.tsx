90d9b367aa064c318b3fd2a860f24651af4b7330
bukxy <peron.nicolas2@gmail.com>
2026-01-16 14:46:22 +0100

    fix: Add check for SidebarMenuButton isActive

--- resources/js/components/front/nav-main.tsx
+++ resources/js/components/front/nav-main.tsx
@@ -8,21 +8,25 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
     return (
         <SidebarGroup className="overflow-hidden px-2 py-0">
             <SidebarMenu className="h-dvh items-center justify-center">
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton
-                            asChild
-                            isActive={page.url.startsWith(resolveUrl(item.href))}
-                            tooltip={{ children: item.title }}
-                            className={'flex flex-col items-center'}
-                        >
-                            <Link href={item.href} prefetch className="h-20 w-20">
-                                {item.icon && <item.icon className="icon-size-front-vav-menu" />}
-                                <span className="text-2xl">{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
+                {items.map((item) => {
+                    const target = item.href ? resolveUrl(item.href) : '';
+                    const isActive = target ? page.url.startsWith(target) : false;
+                    return (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                asChild
+                                isActive={isActive}
+                                tooltip={{ children: item.title }}
+                                className={'flex flex-col items-center'}
+                            >
+                                <Link href={item.href} prefetch className="h-20 w-20">
+                                    {item.icon && <item.icon className="icon-size-front-vav-menu" />}
+                                    <span className="text-2xl">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    )
+                })}
             </SidebarMenu>
         </SidebarGroup>
     );
