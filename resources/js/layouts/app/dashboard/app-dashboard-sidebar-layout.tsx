d8aaf7b7a2c256041bfd5907132cd8766e96d67a
bukxy <peron.nicolas2@gmail.com>
2026-01-16 14:40:36 +0100

    feat: Add AppDashboardSidebarLayout

---
+++ resources/js/layouts/app/dashboard/app-dashboard-sidebar-layout.tsx
@@ -0,0 +1,22 @@
+import { DashboardContent } from '@/components/dashboard/dashboard-content';
+import { DashboardShell } from '@/components/dashboard/dashboard-shell';
+import { DashboardSidebar } from '@/components/dashboard/dashboard-sidebar';
+import { DashboardSidebarHeader } from '@/components/dashboard/dashboard-sidebar-header';
+import { type BreadcrumbItem } from '@/types';
+import { type PropsWithChildren } from 'react';
+
+export default function AppDashboardSidebarLayout({
+    children,
+    breadcrumbs = [],
+}: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+    return (
+        <DashboardShell variant="sidebar">
+            <DashboardSidebar />
+            <DashboardContent variant="sidebar" className="overflow-x-hidden">
+                <DashboardSidebarHeader breadcrumbs={breadcrumbs} />
+                {children}
+            </DashboardContent>
+        </DashboardShell>
+    );
+}
+
