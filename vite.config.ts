9cb6c592c78ae583d61dad97ec93f8d77f637914
bukxy <peron.nicolas2@gmail.com>
2026-01-28 20:46:55 +0100

    feat: Add vite hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,16 @@ export default defineConfig({
     esbuild: {
         jsx: 'automatic',
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        watch: {
+            usePolling: true,
+            interval: 100,
+        },
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
 });
