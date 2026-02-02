f02c81cd6a54a0766441f1bf4d34dd716aeb1d3f
bukxy <peron.nicolas2@gmail.com>
2026-02-02 18:39:07 +0100

    refactor: Add Resource/ResourceCollection interfaces usages

--- resources/js/pages/dashboard/votes_websites/add_edit.tsx
+++ resources/js/pages/dashboard/votes_websites/add_edit.tsx
@@ -7,35 +7,20 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 import { Spinner } from '@/components/ui/spinner';
-import AppLayout from '@/layouts/app-layout';
-import vote from '@/routes/dashboard/vote';
 import voteWebsite from '@/routes/dashboard/vote-website';
-import { BreadcrumbItem, TableColumn } from '@/types';
+import { BreadcrumbItem, Resource, ResourceCollection, TableColumn } from '@/types';
 import { Form, Head, router } from '@inertiajs/react';
 import { useState } from 'react';
 import { VoteWebsite, VoteWebsiteForm } from '@/types/vote-website';
 import {Trash} from "lucide-react";
 
-const columnsVoteWebsite: TableColumn<VoteWebsite>[] = [
+const columns: TableColumn<VoteWebsite>[] = [
     { key: 'id', label: 'ID', width: 'w-10' },
     { key: 'name', label: 'Name', width: 'w-56' },
+    { key: 'logo', label: 'Logo', width: 'w-56', type: 'image' },
     { key: 'url', label: 'Url', width: 'w-56' },
-    {
-        key: 'created_at',
-        label: 'Created at',
-        width: 'w-32',
-        type: 'date',
-        hms: true,
-    },
-    { key: 'created_by', label: 'Created by', width: 'w-32' },
-    {
-        key: 'updated_at',
-        label: 'Updated at',
-        width: 'w-32',
-        type: 'date',
-        hms: true,
-    },
-    { key: 'updated_by', label: 'Updated by', width: 'w-32' },
+    { key: 'created', label: 'Created at', width: 'w-32', type: 'user' },
+    { key: 'updated', label: 'Updated by', width: 'w-32', type: 'user' },
     { key: 'is_enabled', label: 'Is active', width: 'w-10', type: 'boolean' },
     {
         key: 'has_verification',
@@ -45,31 +30,27 @@ const columnsVoteWebsite: TableColumn<VoteWebsite>[] = [
     },
 ];
 
-export default function Vote({ data, votes_websites }: { data: VoteWebsite | null; votes_websites: VoteWebsite[] }) {
-
-    const voteWebsiteId = data?.id;
+export default function Vote({ data, votes_websites }: { data: Resource<VoteWebsite> | null; votes_websites: ResourceCollection<VoteWebsite> }) {
+    const itemData = data?.data ?? null;
+    const itemId = itemData?.id;
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
-            title: 'Vote',
-            href: vote.list().url,
-        },
-        {
-            title: voteWebsiteId ? `Edit vote website` : 'New vote website',
+            title: itemId ? `Edit vote website` : 'New vote website',
             href: voteWebsite.add().url,
         },
     ];
 
-    const initData = (data?: VoteWebsite): VoteWebsiteForm => ({
-        name: data?.name ?? '',
-        url: data?.url ?? '',
-        is_enabled: data?.is_enabled ?? false,
-        has_verification: data?.has_verification ?? false,
-        verification_key: data?.verification_key ?? '',
+    const initData = (itemData?: VoteWebsite): VoteWebsiteForm => ({
+        name: itemData?.name ?? '',
+        url: itemData?.url ?? '',
+        is_enabled: itemData?.is_enabled ?? false,
+        has_verification: itemData?.has_verification ?? false,
+        verification_key: itemData?.verification_key ?? '',
         logo: null,
     });
 
-    const [formData, setFormData] = useState<VoteWebsiteForm>(() => initData(data ?? undefined));
+    const [formData, setFormData] = useState<VoteWebsiteForm>(() => initData(itemData ?? undefined));
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value, type, checked, files } = e.target;
@@ -80,21 +61,21 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
     };
 
     const formConfig =
-        voteWebsiteId !== undefined ? voteWebsite.editStore.form({ id: voteWebsiteId }) : voteWebsite.addStore.form();
+        itemId !== undefined ? voteWebsite.editStore.form({ id: itemId }) : voteWebsite.addStore.form();
 
     return (
         <>
-            <Head title={breadcrumbs[1].title} />
+            <Head title={breadcrumbs[0].title} />
 
             <div>
                 <div className="rounded-xl border border-sidebar-border/70 p-4 dark:border-sidebar-border">
-                    <Heading title={voteWebsiteId ? `${breadcrumbs[1].title} : ${data?.name}` : breadcrumbs[1].title} />
+                    <Heading title={itemId ? `${breadcrumbs[0].title} : ${itemData?.name}` : breadcrumbs[0].title} />
                     <div className="flex w-full flex-col gap-6 lg:flex-row">
                         <Form
                             {...formConfig}
                             resetOnSuccess={false}
                             onSuccess={() => {
-                                if (!voteWebsiteId) {
+                                if (!itemId) {
                                     setFormData({
                                         name: '',
                                         url: '',
@@ -226,7 +207,7 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
                                             autoComplete="logo"
                                         />
                                         <InputError message={errors.logo} />
-                                        {data?.logo && voteWebsiteId && (
+                                        {itemId != null && itemData?.logo && (
                                             <div className="grid w-full grid-cols-2 items-center justify-center">
                                                 <div className="me-5 flex justify-center">
                                                     <span className={`me-5`}>Actual logo :</span>
@@ -234,9 +215,7 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
                                                         type="button"
                                                         onClick={() => {
                                                             if (confirm('Are you sure to delete the logo ?')) {
-                                                                router.delete(
-                                                                    voteWebsite.deleteLogo(voteWebsiteId).url,
-                                                                );
+                                                                router.delete(voteWebsite.deleteLogo(itemId).url);
                                                             }
                                                         }}
                                                     >
@@ -244,8 +223,8 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
                                                     </button>
                                                 </div>
                                                 <img
-                                                    alt={String(data.logo.path)}
-                                                    src={String(data.logo.path)}
+                                                    alt={String(itemData.logo.path)}
+                                                    src={String(itemData.logo.path)}
                                                     className={'m-auto h-16 w-16 rounded-full'}
                                                 />
                                             </div>
@@ -260,8 +239,9 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
                                         data-test="create-vote-website-button"
                                     >
                                         {processing && <Spinner />}
-                                        {voteWebsiteId ? 'S A V E' : 'C R E A T E'}
+                                        {itemId ? 'S A V E' : 'C R E A T E'}
                                     </Button>
+                                    <InputError message={errors.voteWebsite} />
                                 </>
                             )}
                         </Form>
@@ -269,7 +249,14 @@ export default function Vote({ data, votes_websites }: { data: VoteWebsite | nul
                         <Separator className="lg:hidden" />
 
                         <div className="overflow-hidden">
-                            <Table data={votes_websites} columns={columnsVoteWebsite} />
+                            <Table
+                                data={votes_websites.data}
+                                columns={columns}
+                                config={{
+                                    editUrl: voteWebsite.edit.definition.url,
+                                    deleteUrl: voteWebsite.delete.definition.url,
+                                }}
+                            />
                         </div>
                     </div>
                 </div>
